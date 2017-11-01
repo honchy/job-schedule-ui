255c2e5140a5bccf971627099106464e07f4ee1e
ghq甘红旗 <hqgan@ctrip.com>
2017-11-01 11:20:52 +0800

    update model

--- src/models/jobInfo.js
+++ src/models/jobInfo.js
@@ -1,14 +1,14 @@
 import { observable } from 'mobx'
 
 export default class JobInfo {
-	id = '';
-	job_code = '';
-	app_name = '';
-	bean_name = '';
-	method_name = '';
-	cron_param = '';
-	schedule_addr = '';
-	param = '';
-	create_time = '';
-	update_time = '';
+	id ;
+	job_code ;
+	app_name ;
+	bean_name ;
+	method_name ;
+	cron_param ;
+	schedule_addr ;
+	param ;
+	create_time ;
+	update_time ;
 }
